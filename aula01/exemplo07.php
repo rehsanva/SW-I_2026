b692dd82d5b6b8119610b2c72439055d4b765166
Rebeca <rebeca.sanva318@gmail.com>
2026-02-22 12:28:39 -0300

    Enhance example with comments on switch statement
    Added comments explaining the switch statement usage in PHP.

--- aula01/exemplo07.php
+++ aula01/exemplo07.php
@@ -1,18 +1,37 @@
 <?php
 
-    $dia ="terça";
-    switch($dia) {
-        case"segunda":
-            echo "Inicio da Semana";
-            break;
-        case"sexta":
-            echo "fim de semana chegando";
-            break;
+/*
+===========================
+📘 SWITCH (igual vários IF)
+===========================
 
-        default:
-            echo"Dia normal.";
-    }
+Usa quando testa UMA variável
+com vários valores.
 
+Python:
+if dia == "segunda"
 
+PHP:
+case "segunda"
+
+break → para o switch
+default → igual ao else
+*/
+
+$dia = "terça";
+
+switch($dia){
+
+    case "segunda":
+        echo "Inicio da Semana";
+        break;
+
+    case "sexta":
+        echo "fim de semana chegando";
+        break;
+
+    default:
+        echo "Dia normal.";
+}
 
 ?>
