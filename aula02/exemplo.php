1fae731f959d95452ef538b212ef6fc445a108c0
Rebeca <rebeca.sanva318@gmail.com>
2026-02-27 16:18:50 -0300

    Add files via upload

---
+++ aula02/exemplo.php
@@ -0,0 +1,17 @@
+<?php
+
+    $a = 0;
+    $b = 4;
+
+    if ($b==0) {
+        echo "Digite um numero diferente de zero";
+    }
+
+    else {
+        $c = $a / $b;
+        echo "$c";
+    }
+   
+    echo "<br>";
+    echo "Continua o programa";
+?>
---
+++ aula02/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 02 - PHP + HTML</title>
+</head>
+<body>
+    <h1>Aula 02 - PHP + HTML </h1>
+    <?php
+        $nome = "Rosana";
+        echo "<p style=\"color:red\">Bem Vindo... $nome</p>";
+
+        /*
+        Intercala entr e as áspas 
+        echo "<p style='color:red'>Bem Vindo... $nome</p>";
+        */
+
+        //laço de repetição - contar de 1 a 5
+
+
+        echo "<ul>";
+        for ($i=0; $i < 12; $i++) { 
+            echo "<li> $i </li>";
+        }
+
+    ?>
+
+<ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+    <li>5</li>
+<ul>
+
+<table>
+    <c
+</body>
+</html>
